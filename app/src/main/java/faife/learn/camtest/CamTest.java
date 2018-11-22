c43935d11521363c62309f8c3ec846e9c7cd73db
faife <faife1979@gmx.net>
2018-11-22 21:35:10 +0100

    Empty project. @myself GOGOGO

---
+++ app/src/main/java/faife/learn/camtest/CamTest.java
@@ -0,0 +1,12 @@
+package faife.learn.camtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CamTest extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
